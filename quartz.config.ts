f73874268754126fe74380445ca1b3a511c187c5
Xindan Zhang <winifredwsig@outlook.com>
2025-02-04 02:53:14 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Special Elite",
+        header: "Lavishly Yours",
         body: "EB Garamond",
         code: "IBM Plex Mono",
       },
