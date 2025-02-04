73ddbd1438a223a7b951274477b191c1ce5ababa
Xindan Zhang <winifredwsig@outlook.com>
2025-02-04 02:55:48 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Lavishly Yours",
+        header: "Mrs Saint Delafield",
         body: "EB Garamond",
         code: "IBM Plex Mono",
       },
